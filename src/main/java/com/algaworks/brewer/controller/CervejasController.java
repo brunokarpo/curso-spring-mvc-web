1b53f728e89dd43837672097cb6922048d2e50d2
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2016-09-01 11:03:09 -0300

    Aula 4 - Parte 1 - Criando primeiro formulári para testar submissão
    Capturando método post.

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -2,6 +2,7 @@ package com.algaworks.brewer.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Controlador de Cervejas. Utilizado para buscar o pacote onde as classes de controller estar&atilde;o. N&atilde;o apagar
@@ -10,9 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CervejasController {
 
-	@RequestMapping("cervejas/novo") // O que o usuário passa na URL
+	@RequestMapping("/cervejas/novo") // O que o usuário passa na URL
 	public String novo() {
 		return "cerveja/CadastroCerveja"; // pagina HTML que sera devolvida para o usuario
+	}
+
+	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+	public String cadastrar() {
+		System.out.println(" >>>>>> Cadastrado!");
+		return "cerveja/CadastroCerveja";
 	}
 
 }
