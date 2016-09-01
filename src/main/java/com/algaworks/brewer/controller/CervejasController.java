5d0523d3dca64c8ebcb04c9e08c27b826cf7e192
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2016-09-01 11:14:11 -0300

    Aula 4 - Parte 3 - Aprendendo como o Spring trabalha com parametros
    Aprendendo como o Spring recebe parâmetros no Endpoint

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.algaworks.brewer.model.Cerveja;
+
 /**
  * Controlador de Cervejas. Utilizado para buscar o pacote onde as classes de controller estar&atilde;o. N&atilde;o apagar
  *
@@ -17,8 +19,9 @@ public class CervejasController {
 	}
 
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(String sku) {
-		System.out.println(" >>>>>> sku: " + sku);
+	public String cadastrar(Cerveja cerveja) {
+		System.out.println(" >>>>>> sku: " + cerveja.getSku());
+		System.out.println(" >>>>>> nome: " + cerveja.getNome());
 		return "cerveja/CadastroCerveja";
 	}
 
