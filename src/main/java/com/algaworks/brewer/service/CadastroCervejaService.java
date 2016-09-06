2ddb3ab30f69cf4fbb5544105ec129f51b78b8ad
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2016-09-06 14:53:47 -0300

    Aula 30 - Parte 2 - Habilitando o gerenciamento manual das transações.
    Agora quem gerencia a transação é o programador adicionando a anotação @Transactional na classe de serviço.

--- src/main/java/com/algaworks/brewer/service/CadastroCervejaService.java
+++ src/main/java/com/algaworks/brewer/service/CadastroCervejaService.java
@@ -2,6 +2,7 @@ package com.algaworks.brewer.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.brewer.model.Cerveja;
 import com.algaworks.brewer.repository.Cervejas;
@@ -12,6 +13,7 @@ public class CadastroCervejaService {
 	@Autowired
 	private Cervejas cervejas;
 
+	@Transactional
 	public void salvar(Cerveja cerveja) {
 		cervejas.save(cerveja);
 	}
