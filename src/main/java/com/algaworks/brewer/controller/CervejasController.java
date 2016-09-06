a2a45c71e3963e8144367834c22568642a8525b0
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2016-09-06 15:34:59 -0300

    Aula 31 - Parte 3 - Criando mensagens de sucesso e modularizando
    Criando modulo de mensagem de sucesso.

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -2,8 +2,6 @@ package com.algaworks.brewer.controller;
 
 import javax.validation.Valid;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
