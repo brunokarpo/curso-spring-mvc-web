45a56aef35a33fad82b02372f753a73e6274a67b
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2016-09-06 09:40:50 -0300

    Aula 26 - Parte 1 - Preenchendo dinamicamente combo de sabores
    Passando combo de sabores para a view reenderizar.

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -2,16 +2,16 @@ package com.algaworks.brewer.controller;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
-import com.algaworks.brewer.repository.Cervejas;
+import com.algaworks.brewer.model.Sabor;
 
 /**
  * Controlador de Cervejas. Utilizado para buscar o pacote onde as classes de controller estar&atilde;o. N&atilde;o apagar
@@ -20,20 +20,16 @@ import com.algaworks.brewer.repository.Cervejas;
 @Controller
 public class CervejasController {
 
-	@Autowired
-	private Cervejas cervejas;
-
 	@RequestMapping("/cervejas/novo") // O que o usuário passa na URL
-	public String novo(Cerveja cerveja) {
-		cervejas.findBySku("AAA1111"); // Apagar ...
+	public ModelAndView novo(Cerveja cerveja) {
+		ModelAndView mv = new ModelAndView("cerveja/CadastroCerveja");
+		mv.addObject("sabores", Sabor.values());
 
-		cervejas.findBySkuIgnoreCase("aa2222"); // Apagar ...
-
-		return "cerveja/CadastroCerveja"; // pagina HTML que sera devolvida para o usuario
+		return mv; // pagina HTML que sera devolvida para o usuario
 	}
 
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		if(result.hasErrors()) {
 			return novo(cerveja);
 		}
@@ -43,7 +39,7 @@ public class CervejasController {
 		System.out.println(" >>>>>> sku: " + cerveja.getSku());
 		System.out.println(" >>>>>> nome: " + cerveja.getNome());
 		System.out.println(" >>>>>> descricao: " + cerveja.getDescricao());
-		return "redirect:/cervejas/novo"; // Em redirect usa a URL e nao o nome da View (html na pasta template)
+		return new ModelAndView("redirect:/cervejas/novo"); // Em redirect usa a URL e nao o nome da View (html na pasta template)
 	}
 
 }
