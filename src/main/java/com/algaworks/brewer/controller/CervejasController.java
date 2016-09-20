14fa796b6ea0c2ec14de56bb5ce84af93a4e4634
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2016-09-20 08:07:35 -0300

    Aula 57 - Parte 1 - Criando nova página para pesquisa de cervejas
    Criando nova tela para pesquisa de cervejas. 
    Adicionando botões nos headers da página para que seja possível navegar entre cadastro e pesquisa de cervejas.

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -53,6 +54,12 @@ public class CervejasController {
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso");
 
 		return new ModelAndView("redirect:/cervejas/novo"); // Em redirect usa a URL e nao o nome da View (html na pasta template)
+	}
+
+	@GetMapping
+	public ModelAndView pesquisar() {
+		ModelAndView mv = new ModelAndView("cerveja/PesquisaCervejas");
+		return mv;
 	}
 
 }
