9b322bb6e03549db57700b8603f46825a7135072
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2016-09-26 10:22:34 -0300

    Aula 79 - Parte 2 - Criando entidade de cliente e seus relacionamentos.

---
+++ src/main/java/com/algaworks/brewer/model/Cliente.java
@@ -0,0 +1,78 @@
+package com.algaworks.brewer.model;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente extends Entidade {
+
+	private static final long serialVersionUID = 3437572685158571977L;
+
+	private String nome;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "tipo_pessoa")
+	private TipoPessoa tipoPessoa;
+
+	@Column(name = "cpf_cnpj")
+	private String cpfOuCnpj;
+	private String telefone;
+	private String email;
+
+	@Embedded
+	private Endereco endereco;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public TipoPessoa getTipoPessoa() {
+		return tipoPessoa;
+	}
+
+	public void setTipoPessoa(TipoPessoa tipoPessoa) {
+		this.tipoPessoa = tipoPessoa;
+	}
+
+	public String getCpfOuCnpj() {
+		return cpfOuCnpj;
+	}
+
+	public void setCpfOuCnpj(String cpfOuCnpj) {
+		this.cpfOuCnpj = cpfOuCnpj;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+}
